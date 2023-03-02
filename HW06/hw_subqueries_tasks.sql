4960aaf223d9abcb1319ff82c2794fb0b561961d
gdubovitskiy <gdubovitskiy@ya.ru>
2023-03-02 18:17:47 +0300

    [HW06] Добавил решение 1 задачи

--- HW06/hw_subqueries_tasks.sql
+++ HW06/hw_subqueries_tasks.sql
@@ -30,7 +30,11 @@ USE WideWorldImporters
 Продажи смотреть в таблице Sales.Invoices.
 */
 
-TODO: напишите здесь свое решение
+SELECT p.PersonID, p.FullName
+  FROM Application.People p
+ WHERE p.IsSalesperson = 1
+   AND p.IsSalesperson NOT IN (SELECT DISTINCT i.SalespersonPersonID FROM Sales.Invoices i WHERE i.InvoiceDate = '20150704')
+ORDER BY p.PersonID;
 
 /*
 2. Выберите товары с минимальной ценой (подзапросом). Сделайте два варианта подзапроса. 
@@ -52,44 +56,5 @@ TODO: напишите здесь свое решение
 входящие в тройку самых дорогих товаров, а также имя сотрудника, 
 который осуществлял упаковку заказов (PackedByPersonID).
 */
-
-TODO: напишите здесь свое решение
-
--- ---------------------------------------------------------------------------
--- Опциональное задание
--- ---------------------------------------------------------------------------
--- Можно двигаться как в сторону улучшения читабельности запроса, 
--- так и в сторону упрощения плана\ускорения. 
--- Сравнить производительность запросов можно через SET STATISTICS IO, TIME ON. 
--- Если знакомы с планами запросов, то используйте их (тогда к решению также приложите планы). 
--- Напишите ваши рассуждения по поводу оптимизации. 
-
--- 5. Объясните, что делает и оптимизируйте запрос
-
-SELECT 
-	Invoices.InvoiceID, 
-	Invoices.InvoiceDate,
-	(SELECT People.FullName
-		FROM Application.People
-		WHERE People.PersonID = Invoices.SalespersonPersonID
-	) AS SalesPersonName,
-	SalesTotals.TotalSumm AS TotalSummByInvoice, 
-	(SELECT SUM(OrderLines.PickedQuantity*OrderLines.UnitPrice)
-		FROM Sales.OrderLines
-		WHERE OrderLines.OrderId = (SELECT Orders.OrderId 
-			FROM Sales.Orders
-			WHERE Orders.PickingCompletedWhen IS NOT NULL	
-				AND Orders.OrderId = Invoices.OrderId)	
-	) AS TotalSummForPickedItems
-FROM Sales.Invoices 
-	JOIN
-	(SELECT InvoiceId, SUM(Quantity*UnitPrice) AS TotalSumm
-	FROM Sales.InvoiceLines
-	GROUP BY InvoiceId
-	HAVING SUM(Quantity*UnitPrice) > 27000) AS SalesTotals
-		ON Invoices.InvoiceID = SalesTotals.InvoiceID
-ORDER BY TotalSumm DESC
-
--- --
 
 TODO: напишите здесь свое решение
